893ae5123719e690e0753150821fa20d0b23f90f
ogaamyr <aamir@ogaamyr.com>
2022-04-25 03:08:39 +0100

    loading screen + refactored product to be rendered with product card

--- frontend/src/components/ShowTool.js
+++ frontend/src/components/ShowTool.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import ProductService from "../services/ProductService";
+import ProductCard from "./ProductCard";
+import LoadingScreen from "./LoadingScreen";
 
 function ShowTool() {
   const navigate = useNavigate();
@@ -44,40 +46,13 @@ function ShowTool() {
         </button>
       </div>
       <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 md:gap-2 p-2">
-        {loading
-          ? "Loading..."
-          : tools.map((product) => {
-              return (
-                <div
-                  className="w-full max-w-sm mx-auto rounded-md shadow-sm hover:shadow-md overflow-hidden"
-                  key={product.name}
-                >
-                  <div className="flex items-end justify-center w-full">
-                    <img
-                      className="object-fill"
-                      // src={"/images/products/placeholder.png"}
-                      src={
-                        "https://res.cloudinary.com/patch-gardens/image/upload/c_fill,f_auto,h_800,q_auto:good,w_800/v1564154462/products/kentia-palm-1a5d98.jpg"
-                      }
-                      alt={product.name}
-                    ></img>
-                  </div>
-                  <div className="flex flex-col px-5 py-3">
-                    <h3 className="text-gray-700 uppercase">{product.name}</h3>
-                    <span className="text-gray-500 mt-2">
-                      ${product.unitPrice}
-                    </span>
-
-                    <button
-                      className="rounded text-white  bg-green-700 hover:bg-green-900 px-4 py-2 m-1 w-full"
-                      onClick={() => console.log("Add to cart")}
-                    >
-                      Add to Cart
-                    </button>
-                  </div>
-                </div>
-              );
-            })}
+        {loading ? (
+          <LoadingScreen />
+        ) : (
+          tools.map((product) => {
+            return <ProductCard product={product} />;
+          })
+        )}
       </div>
     </div>
   );
