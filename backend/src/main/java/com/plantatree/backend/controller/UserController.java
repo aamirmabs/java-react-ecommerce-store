51307b1c2036dcdb320b27fe583f92b625cd581c
ogaamyr <aamir@ogaamyr.com>
2022-04-26 03:51:12 +0100

    user service now creates an admin role and 3 users

--- backend/src/main/java/com/plantatree/backend/controller/UserController.java
+++ backend/src/main/java/com/plantatree/backend/controller/UserController.java
@@ -3,9 +3,12 @@ package com.plantatree.backend.controller;
 import com.plantatree.backend.entity.User;
 import com.plantatree.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 public class UserController {
@@ -13,8 +16,24 @@ public class UserController {
   @Autowired
   private UserService userService;
 
+  @PostConstruct
+  public void initRolesAndUsers() {
+    userService.initRolesAndUsers();
+  }
+
   @PostMapping({"/api/registerNewUser"})
   public User registerNewUser(@RequestBody User user) {
     return userService.registerNewUser(user);
   }
+
+  @GetMapping({"/api/forAdmin"})
+  public String forAdmin() {
+    return "This URL is for admin only";
+  }
+
+  @GetMapping({"/api/forUser"})
+  public String forUser() {
+    return "This URL is for user only";
+  }
+
 }
--- backend/src/main/java/com/plantatree/backend/service/UserService.java
+++ backend/src/main/java/com/plantatree/backend/service/UserService.java
@@ -1,9 +1,14 @@
 package com.plantatree.backend.service;
 
+import com.plantatree.backend.dao.RoleDao;
 import com.plantatree.backend.dao.UserDao;
+import com.plantatree.backend.entity.Role;
 import com.plantatree.backend.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserService {
@@ -11,8 +16,67 @@ public class UserService {
   @Autowired
   private UserDao userDao;
 
+  @Autowired
+  private RoleDao roleDao;
+
   public User registerNewUser(User user) {
     return userDao.save(user);
+  }
+
+
+  public void initRolesAndUsers() {
+    Role adminRole = new Role();
+    adminRole.setRoleName("Admin");
+    adminRole.setRoleDescription("This is the admin role");
+    roleDao.save(adminRole);
+
+    Role userRole = new Role();
+    userRole.setRoleName("User");
+    userRole.setRoleDescription("Default role for newly craeted users");
+    roleDao.save(userRole);
+
+    User adminUser = new User();
+    adminUser.setUserFirstName("admin");
+    adminUser.setUserLastName("admin");
+    adminUser.setUserName("admin123");
+    adminUser.setUserPassword("admin123");
+    Set<Role> adminRoles = new HashSet<>();
+    adminRoles.add(adminRole);
+    adminUser.setRole(adminRoles);
+    userDao.save(adminUser);
+
+
+    User user1 = new User();
+    user1.setUserFirstName("user1");
+    user1.setUserLastName("user1");
+    user1.setUserName("user1");
+    user1.setUserPassword("user123");
+    Set<Role> userRoles = new HashSet<>();
+    userRoles.add(userRole);
+    user1.setRole(userRoles);
+    userDao.save(user1);
+
+
+    User user2 = new User();
+    user2.setUserFirstName("user2");
+    user2.setUserLastName("user2");
+    user2.setUserName("user2");
+    user2.setUserPassword("user123");
+    // userRoles = new HashSet<>();
+    userRoles.add(userRole);
+    user2.setRole(userRoles);
+    userDao.save(user2);
+
+    User user3 = new User();
+    user3.setUserFirstName("user3");
+    user3.setUserLastName("user3");
+    user3.setUserName("user3");
+    user3.setUserPassword("user123");
+    // userRoles = new HashSet<>();
+    userRoles.add(userRole);
+    user3.setRole(userRoles);
+    userDao.save(user3);
+
   }
 
 }
