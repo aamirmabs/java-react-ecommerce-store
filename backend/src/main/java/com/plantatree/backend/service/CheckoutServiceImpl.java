c2ee649ca271977ea2dcb0494b0a356321903913
ogaamyr <aamir@ogaamyr.com>
2022-04-28 04:30:19 +0100

    hotfix on the service class

--- backend/src/main/java/com/plantatree/backend/service/CheckoutServiceImpl.java
+++ backend/src/main/java/com/plantatree/backend/service/CheckoutServiceImpl.java
@@ -3,8 +3,15 @@ package com.plantatree.backend.service;
 import com.plantatree.backend.dao.CustomerRepository;
 import com.plantatree.backend.dto.Purchase;
 import com.plantatree.backend.dto.PurchaseResponse;
+import com.plantatree.backend.entity.Customer;
+import com.plantatree.backend.entity.Order;
+import com.plantatree.backend.entity.OrderItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
@@ -17,8 +24,40 @@ public class CheckoutServiceImpl implements CheckoutService {
   }
 
   @Override
+  @Transactional
   public PurchaseResponse placeOrder(Purchase purchase) {
-    return null;
+
+    // retrieve order info from dto
+    Order order = purchase.getOrder();
+
+
+    // generate tracking number
+    String orderTrackingNumber = generateOrderTrackingNumber();
+    order.setOrderTrackingNumber(orderTrackingNumber);
+
+
+    // populate order with orderItems
+    Set<OrderItem> orderItems = purchase.getOrderItems();
+    orderItems.forEach(item -> order.add(item));
+
+
+    // populate order with billingAddress and shippingAddress
+    order.setBillingAddress(purchase.getBillingAddress());
+    order.setShippingAddress(purchase.getShippingAddress());
+
+
+    // save to the database
+    Customer customer = purchase.getCustomer();
+    customer.add(order);
+
+
+    // return response
+    return new PurchaseResponse(orderTrackingNumber);
+  }
+
+  private String generateOrderTrackingNumber() {
+    // generate a random UUID
+    return UUID.randomUUID().toString();
   }
 
 }
